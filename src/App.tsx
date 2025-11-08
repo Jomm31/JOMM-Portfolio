af494f67d1228915629eee2e93081ee93fafecba
jomm <joemired@gmail.com>
2025-11-08 18:48:16 +0800

    floating fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Clouds from './components/Clouds.tsx';
 import Header from './components/Header.tsx';
 import Home from './Home.tsx';
-
+import Landscape from './components/Landscape.tsx';
 function App() {
   return (
     <div className="relative w-full min-h-screen bg-blue-100">
--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,11 @@ import { motion } from 'framer-motion';
 import floatingImage from './assets/floating.webp';
 import Landscape from './components/Landscape.tsx';
 
+const FLOATING_DEFAULT = {
+  top: '46%',
+  left: '35%',
+};
+
 function Home() {
   const constraintsRef = useRef<HTMLDivElement>(null);
 
@@ -15,8 +20,8 @@ function Home() {
         dragConstraints={constraintsRef}
         dragElastic={0.1}
         dragTransition={{ bounceStiffness: 300, bounceDamping: 20 }}
-        className="absolute left-1/2 top-1/2 z-10 w-150 cursor-grab active:cursor-grabbing will-change-transform"
-        style={{ x: '-50%', y: '-50%' }}
+        className="absolute z-10 w-150  -translate-x-1/2 -translate-y-1/2 cursor-grab active:cursor-grabbing will-change-transform"
+        style={{ top: FLOATING_DEFAULT.top, left: FLOATING_DEFAULT.left }}
         whileHover={{ scale: 1.05 }}
         whileDrag={{ scale: 1.1 }}
       />
